0982171f82310c2d3fcbc027815086c6922ad751
varunshrikar <varunshrikar1995@gmail.com>
2022-07-04 11:57:06 +0530

    url updated to view app

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     # ex: /
     path('',views.index),
+    # ex:/qno/
+    path('<int:question_id>/',views.detail, name='detail'),
+    # ex: /qno/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /qno/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
